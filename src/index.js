95a37d846b9e5e97271afbe6ec32fdd9ea79466d
Kamel Bourahla <kbourahla@sofgensavetax.com>
2019-04-25 08:33:18 +0200

    add component comment

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}/>
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    Sam
-                </a>
-                <div className="metadata">
-                    <span className="date">Todayx at 5:00PM</span>
-                </div>
-                <div className="text">Nice blog post!</div>
-            </div>
-        </div>
-    </div>);
+      <CommentDetail
+        author="Kamel"
+        timeAgo="Today at 5:00PM"
+        avatar={faker.image.avatar()}
+        comment="Super!"
+      />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:30PM"
+        avatar={faker.image.avatar()}
+        comment="Merci!"
+      />
+      <CommentDetail
+        author="John"
+        timeAgo="Yesterday at 9:00PM"
+        avatar={faker.image.avatar()}
+        comment="Good job"
+      />
+      <CommentDetail
+        author="Bob"
+        timeAgo="Yesterday at 8:00PM"
+        avatar={faker.image.avatar()}
+        comment="Nul!"
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
